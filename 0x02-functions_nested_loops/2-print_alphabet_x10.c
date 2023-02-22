bd0cd1f16d2165cc5d8b636b7b72bf611d376ab8
SnikksG <nickyndex@gmail.com>
2023-02-22 02:14:20 -0800

    nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,18 @@
  */
 void print_alphabet_x10(void)
 {
-        int i = 0;
-        char c;
+	int i = 0;
+	char c;
 
-        while (i < 10)
-        {
-                c = 'a';
-                while (c <= 'z')
-                {
-                        _putchar(c);
-                        c++;
-                }
-                _putchar('\n');
-                i++;
-        }
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
