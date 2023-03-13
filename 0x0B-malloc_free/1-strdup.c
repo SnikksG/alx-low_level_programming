5cd9c84d7b0c4ba0bf22d539d37cb87ff0ce5a24
SnikksG <nickyndex@gmail.com>
2023-03-13 10:14:37 -0700

    correction

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,25 +1,28 @@
-#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <stdlib.h>		
+#include "main.h"
+		
+/**
+ * _strdup - duplicate to new memory space location	
+ * @str: char	
+ * Return: 0		
+ */
+		
 char *_strdup(char *str)
+		
 {
-  int i = 0, j, k = 0;
-  char *newStr = NULL;
-
-  while (str[k] != '\0')
-    {
-      i++;
-      k++;
-    }
-
-  newStr = malloc(sizeof(char) * (i + 1));
-
-  if (str == NULL)
-    return (NULL);
-
-  for (j = 0; j <= i; j++)
-    newStr[j] = str[j];
-
-  return (newStr);
+	char *aaa;
+	int i, r = 0;
+	if (str == NULL)
+		return (NULL);	
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	aaa = malloc(sizeof(char) * (i + 1));
+	if (aaa == NULL)
+		return (NULL);
+	for (r = 0; str[r]; r++)
+		aaa[r] = str[r];		
+	return (aaa);
 }
+
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,20 +1,39 @@
-#include "main.h"
-#include <stdio.h>
+#include "main.h"		
 #include <stdlib.h>
-
+		
+/**
+ * alloc_grid - nested loop to make grid
+ * @width: width input
+ * @height: height input
+ * Return: pointer to 2 dim. array		
+ */
+		
 int **alloc_grid(int width, int height)
+		
 {
-  int i, j;
-  int *ptr = NULL;
-
-  if (width <= 0 || height <= 0)
-    return (NULL);
-
-  ptr = (int *)malloc(width * height * sizeof(int));
-
-  for (i = 0; i < width * height; i++)
-    {
-    }
-
-  return (ptr);
+	int **mee;
+	int x, y;
+	if (width <= 0 || height <= 0)
+		return (NULL)
+	mee = malloc(sizeof(int *) * height);
+	if (mee == NULL)
+		return (NULL);
+	for (x = 0; x < height; x++)	
+	{
+		mee[x] = malloc(sizeof(int) * width);
+		if (mee[x] == NULL)
+		{
+			for (; x >= 0; x--)
+				free(mee[x]);
+			free(mee);
+			return (NULL);
+		}	
+	}
+	for (x = 0; x < height; x++)	
+	{
+		for (y = 0; y < width; y++)
+			mee[x][y] = 0;	
+	}
+	return (mee);		
 }
+
